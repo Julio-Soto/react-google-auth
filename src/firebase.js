1489f654b16f6f8795e353eb180c120c94b814fb
Julio Soto <juliosotodev@gmail.com>
2022-09-22 23:38:36 -0600

    added firebase package and boilerplate code

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHUbf6tauJFQL2GFjPbxnd0TjL0aWFQns",
+  authDomain: "react--auth-e1ef8.firebaseapp.com",
+  projectId: "react--auth-e1ef8",
+  storageBucket: "react--auth-e1ef8.appspot.com",
+  messagingSenderId: "972209033306",
+  appId: "1:972209033306:web:4503bf3cc12d34ecd94863"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
