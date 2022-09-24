285574e625ed1d9b08b7492b8d956345e3a9c612
Julio Soto <juliosotodev@gmail.com>
2022-09-23 23:48:51 -0600

    added react-redux and useSelector

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import {signInWithGoogle} from './firebase.js'
-import { store } from './store'
+import {useSelector} from 'react-redux'
 
 function App() {
+  const name = useSelector(state => state.name)
+  const email = useSelector(state => state.email)
+  const imageURL = useSelector(state => state.imageURL)
+
   return (
     <div className="App">
       <button onClick={signInWithGoogle}>Sign In</button>
-      <h1>{store.getState().name}</h1>
-      <h1>{store.getState().email}</h1>
-      <h1>{store.getState().imageURL}</h1>
-      <img src='store.getState().imageURL' alt='user'></img>
+      <h1>{name}</h1>
+      <h1>{email}</h1>
+      <h1>{imageURL}</h1>
+      <img src={imageURL} alt='user'></img>
     </div>
   );
 }
