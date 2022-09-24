f5f4311d8432a189031036fca2c726ddfe97fffe
Julio Soto <juliosotodev@gmail.com>
2022-09-23 22:20:26 -0600

    added sign in function and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {signInWithGoogle} from './firebase.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Firebase</h1>
+      <button onClick={signInWithGoogle}>Sign In</button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import {getAuth,GoogleAuthProvider,signInWithPopup} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +18,15 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
+export const signInWithGoogle = () => {
+    signInWithPopup(auth,provider)
+    .then(result =>{
+        console.log(result)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
